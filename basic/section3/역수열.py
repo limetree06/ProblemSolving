1077bc747606d044cf37ad72cd66dacdbe68536d
limetree06 <limetree06@unist.ac.kr>
2023-09-27 21:29:34 +0900

    done : greedy,

--- basic/section3/역수열.py
+++ basic/section3/역수열.py
@@ -1,10 +1,55 @@
 import sys
 
+""" 
+1부터 n까지의 수를 한 번씩만 사용하여 이루어진 수열이 있을 때,
+1부터 n까지 각각의 수 앞 에 놓여 있는 자신보다 큰 수들의 개수를 수열로 표현한 것을 역수열이라 한다.
+예를 들어 다음과 같은 수열의 경우
+[4 8 6 2 5 1 3 7]
+1 앞에 놓인 1보다 큰 수는 4, 8, 6, 2, 5. 이렇게 5개이고,
+2 앞에 놓인 2보다 큰 수는 4, 8, 6. 이렇게 3개,
+3 앞에 놓인 3보다 큰 수는 4, 8, 6, 5 이렇게 4개...
+따라서 [4 8 6 2 5 1 3 7] 의 역수열은 [5 3 4 0 2 1 1 0] 이 된다.
+n과 1부터 n까지의 수를 사용하여 이루어진 수열의 역수열이 주어졌을 때, 원래의 수열을 출 력하는 프로그램을 작성하세요.
 
-def func():
-    print()
+▣ 입력설명
+첫 번째 줄에 자연수 N(3<=N<100)이 주어지고, 두 번째 줄에는 역수열이 숫자 사이에 한칸의 공백을 두고 주어진다.
+
+▣ 출력설명 
+원래 수열을 출력합니다.
+
+▣ 입력예제 
+8
+5 3 4 0 2 1 1 0
+
+▣ 출력예제
+4 8 6 2 5 1 3 7 
+"""
+
+
+def normal_list(reverse_list, N):
+    answer = [-1 for _ in range(N)]
+    for index, reverse in enumerate(reverse_list):
+        count = 0
+        blank = 0
+
+        for i in range(0, N):
+            if index + 1 > answer[i] > -1:
+                count = count + 1
+            else:
+                blank = blank + 1
+            if blank == reverse + 1:
+                break
+
+        if reverse == 0 and count == 0:
+            answer[0] = index + 1
+        else:
+            answer[reverse + count] = index + 1
+    return answer
 
 
 for i in range(1, 6):
     sys.stdin = open(f"test/in{i}.txt", "rt")
-    func()
+    N = int(input())
+    reverse_list = list(map(int, input().split()))
+    normal = normal_list(reverse_list, N)
+    print(normal)
--- basic/section3/증가수열.py
+++ basic/section3/증가수열.py
@@ -1,10 +1,63 @@
 import sys
 
+""" 
+1부터 N까지의 모든 자연수로 구성된 길이 N의 수열이 주어집니다.
+이 수열의 왼쪽 맨 끝 숫자 또는 오른쪽 맨 끝 숫자 중 하나를 가져와 나열하여 가장 긴 증가수열 을 만듭니다. 
+이때 수열에서 가져온 숫자(왼쪽 맨 끝 또는 오른쪽 맨 끝)는 그 수열에서 제거됩니 다.
+예를 들어 2 4 5 1 3 이 주어지면 만들 수 있는 가장 긴 증가수열의 길이는 4입니다.
+맨 처음 왼쪽 끝에서 2를 가져오고, 그 다음 오른쪽 끝에서 3을 가져오고, 왼쪽 끝에서 4, 왼쪽끝에서5를가져와 2345 증가수열을만들수있습니다.
 
-def func():
-    print()
+▣ 입력설명
+첫째 줄에 자연수 N(3<=N<=100)이 주어집니다. 두 번째 줄에 N개로 구성된 수열이 주어집니다.
+
+▣ 출력설명
+첫째 줄에 최대 증가수열의 길이를 출력합니다.
+두 번째 줄에 가져간 순서대로 왼쪽 끝에서 가져갔으면 ‘L', 오른쪽 끝에서 가져갔으면 ’R'를 써 간 문자열을 출력합니다.(단 마지막에 남은 값은 왼쪽 끝으로 생각합니다.)
+
+▣ 입력예제
+5
+2 4 5 1 3
+
+▣ 출력예제
+4
+LRLL
+"""
+
+
+def sequence(numbers):
+    new_numbers = list(numbers)
+    answer = ""
+    last = 0
+    while True:
+        left = new_numbers[0]
+        right = new_numbers[-1]
+        if right >= last and left >= last:
+            if left >= right >= last:
+                last = new_numbers.pop()
+                answer = answer + "R"
+
+            elif right >= left >= last:
+                last = new_numbers.pop(0)
+                answer = answer + "L"
+
+        elif right >= last >= left or left >= last >= right:
+            if right >= last:
+                last = new_numbers.pop()
+                answer = answer + "R"
+
+            elif left >= last:
+                last = new_numbers.pop(0)
+                answer = answer + "L"
+
+        elif last >= left and last >= right:
+            break
+
+    print(len(answer))
+    print(answer)
 
 
 for i in range(1, 6):
     sys.stdin = open(f"test/in{i}.txt", "rt")
-    func()
+    N = int(input())
+    numbers = list(map(int, input().split()))
+    sequence(numbers)
