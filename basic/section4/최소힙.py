13a3e3c3fed1da040b7f498b79ff2446dcd2805d
limetree06 <limetree06@unist.ac.kr>
2023-09-28 22:54:41 +0900

    done : stack, hash, queue, heap

--- basic/section4/최소힙.py
+++ basic/section4/최소힙.py
@@ -1,9 +1,29 @@
 import sys
+import heapq as hp
+
+""" 
+heapq라이브러리 안쓰고 한번 구현해볼껏!
+
+"""
 
 
-def func():
-    return 0
+def heap_tree(numbers):
+    min_tree = []
+    for num in numbers:
+        if num == 0:
+            print(hp.heappop(min_tree))
+        else:
+            hp.heappush(min_tree, num)
 
 
-for i in range(1, 6):
+for i in range(2, 3):
+    print(f"--------{i}문제----------")
     sys.stdin = open(f"test/in{i}.txt", "rt")
+    numbers = []
+    while True:
+        num = int(input())
+        if num == -1:
+            break
+        else:
+            numbers.append(num)
+    heap_tree(numbers)
