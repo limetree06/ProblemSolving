00fa274ae0a6db1446044da290bce820c0501e02
limetree06 <limetree06@unist.ac.kr>
2023-10-14 18:40:25 +0900

    231014 : binary search

--- programmers/고득점kit/탐색모음(BFS,DFS,BF,이분탐색)/징검다리.py
+++ programmers/고득점kit/탐색모음(BFS,DFS,BF,이분탐색)/징검다리.py
@@ -1,1 +1,24 @@
 #이분탐색
+def solution(distance, rocks, n):
+    N = len(rocks) - n +1
+    rocks.append(distance)
+    rocks.sort()
+    
+    min_value = 0
+    max_value = distance
+    while min_value <= max_value:
+        mid = (min_value + max_value) // 2
+        count = 1
+        start = mid
+        for r in rocks:
+            if start == r:
+                count += 1
+                start += mid
+            elif start < r:
+                start = r + mid
+                count += 1
+        if count > N :
+            min_value = mid + 1
+        else:
+            max_value = mid - 1
+    return max_value
